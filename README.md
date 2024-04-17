320d824d44fbfa8d94e84bca03e2b5f579dce885
latamrisus <swag@swag.swag>
2024-04-17 23:00:37 +0300

    Fronted

--- README.md
+++
@@ -1,2 +0,0 @@
-# WebProject
-This project is a secure Java web application using Spring Boot, featuring user and blog post management, and JWT-based authentication.
---
+++ frontend/src/components/main/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <IndexPost v-for="post in posts" :post="post" :key="post.id"/>
+    </div>
+</template>
+
+<script>
+import IndexPost from "@/components/main/IndexPost.vue";
+
+export default {
+    name: "Index",
+    props: ["posts"],
+  components: {
+      IndexPost
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
